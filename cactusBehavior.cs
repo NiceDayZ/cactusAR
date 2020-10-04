75b88455c277b38ada30752b652fb3d311275e98
NiceDayZ <craciunmihai40@gmail.com>
2020-10-04 13:05:31 +0300

    cactus AR

---
+++ cactusBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cactusBehavior : MonoBehaviour
+{
+    public Animator animator;
+    protected AnimatorOverrideController animatorOverrideController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){
+        float Dist = Vector3.Distance(Camera.main.transform.position,transform.position);
+        Debug.Log(Dist);
+        Debug.Log(animator.GetBool("cameraIsClose"));
+        if(Dist < 5000f){
+            animator.SetBool("cameraIsClose", true);
+        }else{
+            animator.SetBool("cameraIsClose", false);
+        }
+    }
+}
